8af1b286881df95ce2f5dec34085472e6588f390
FelipeFrug <103456962+FelipeFrug@users.noreply.github.com>
2024-08-19 11:52:20 -0300

    Update main.c

--- exe3/main.c
+++ exe3/main.c
@@ -1,39 +1,43 @@
+
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-
-const int LED_PIN1 = 4;
-const int LED_PIN2 = 6;
-const int BTN_PIN1 = 28;
-const int BTN_PIN2 = 26;
+const int LED_PIN_1 = 4;
+const int LED_PIN_2 = 6;
+const int BTN_PIN_1 = 28;
+const int BTN_PIN_2 = 26;
 
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN_PIN1);
-  gpio_set_dir(BTN_PIN1, GPIO_IN);
-  gpio_pull_up(BTN_PIN1);
+  gpio_init(LED_PIN_1);
+  gpio_init(LED_PIN_2);
+  gpio_init(BTN_PIN_1);
+  gpio_init(BTN_PIN_2);
 
-  gpio_init(BTN_PIN2);
-  gpio_set_dir(BTN_PIN2, GPIO_IN);
-  gpio_pull_up(BTN_PIN2);
+  gpio_set_dir(LED_PIN_1, GPIO_OUT);
+  gpio_set_dir(LED_PIN_2, GPIO_OUT);
+  gpio_set_dir(BTN_PIN_1, GPIO_IN);
+  gpio_set_dir(BTN_PIN_2, GPIO_IN);
 
-  gpio_init(LED_PIN1);
-  gpio_init(LED_PIN2);
-  gpio_set_dir(LED_PIN1, GPIO_OUT);
-  gpio_set_dir(LED_PIN2, GPIO_OUT);
+  gpio_pull_up(BTN_PIN_1);
+  gpio_pull_up(BTN_PIN_2);
 
   while (true) {
-    int luz = gpio_get(LED_PIN_R);
-    gpio_put(LED_PIN_R, !luz);
+  if (!gpio_get(BTN_PIN_1)) {
+    int luz = gpio_get(LED_PIN_1);
+    gpio_put(LED_PIN_1, !luz);
     sleep_ms(50);
-    while (!gpio_get(BTN_PIN_R)) {
+    while (!gpio_get(BTN_PIN_1)) {
     };
-    int luz2 = gpio_get(LED_PIN_G);
-    gpio_put(LED_PIN_G, !luz2);
+  };
+  if (!gpio_get(BTN_PIN_2)) {
+    int luz = gpio_get(LED_PIN_2);
+    gpio_put(LED_PIN_2, !luz);
     sleep_ms(50);
-    while (!gpio_get(BTN_PIN_G)) {
+    while (!gpio_get(BTN_PIN_2)) {
     }
   }
+}
 }
