12a8a9b0539b8d85589b4114088094e41d349384
FelipeFrug <103456962+FelipeFrug@users.noreply.github.com>
2024-08-19 10:22:32 -0300

    Update main.c

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,34 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+
+const int LED_PIN1 = 4;
+const int LED_PIN2 = 6;
+const int BTN_PIN1 = 28;
+const int BTN_PIN2 = 26;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(BTN_PIN1);
+  gpio_set_dir(BTN_PIN1, GPIO_IN);
+  gpio_pull_up(BTN_PIN1);
+
+  gpio_init(BTN_PIN2);
+  gpio_set_dir(BTN_PIN2, GPIO_IN);
+  gpio_pull_up(BTN_PIN2);
+
+  gpio_init(LED_PIN1);
+  gpio_init(LED_PIN2);
+  gpio_set_dir(LED_PIN1, GPIO_OUT);
+  gpio_set_dir(LED_PIN2, GPIO_OUT);
+
   while (true) {
+    if (!gpio_get(BTN_PIN1)) {
+      gpio_put(LED_PIN1, 1);
+    }
+    if (!gpio_get(BTN_PIN2)) {
+      gpio_put(LED_PIN2, 1);
+    }
   }
 }
