4e9d5df9e26538edb73b8b3c5f46c47200ef0298
FelipeFrug <103456962+FelipeFrug@users.noreply.github.com>
2024-08-19 11:54:15 -0300

    Update main.c

--- exe4/main.c
+++ exe4/main.c
@@ -1,48 +1,50 @@
+
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_1 = 5;
+const int LED_PIN_2 = 8;
+const int LED_PIN_3 = 11;
+const int LED_PIN_4 = 15;
+const int BTN_PIN = 28;
 
-const int LED_PIN1 = 7;
-const int LED_PIN2 = 11;
-const int LED_PIN3 = 15;
-const int LED_PIN4 = 19;
-const int BTN_PIN1 = 34;
 
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN_PIN1);
-  gpio_set_dir(BTN_PIN1, GPIO_IN);
-  gpio_pull_up(BTN_PIN1);
 
-  gpio_init(LED_PIN1);
-  gpio_init(LED_PIN2);
-  gpio_init(LED_PIN3);
-  gpio_init(LED_PIN4);
-  gpio_set_dir(LED_PIN1, GPIO_OUT);
-  gpio_set_dir(LED_PIN2, GPIO_OUT);
-  gpio_set_dir(LED_PIN3, GPIO_OUT);
-  gpio_set_dir(LED_PIN4, GPIO_OUT);
+  gpio_init(LED_PIN_1);
+  gpio_init(LED_PIN_2);
+  gpio_init(LED_PIN_3);
+  gpio_init(LED_PIN_4);
+  gpio_init(BTN_PIN);
+
+  gpio_set_dir(LED_PIN_1, GPIO_OUT);
+  gpio_set_dir(LED_PIN_2, GPIO_OUT);
+  gpio_set_dir(LED_PIN_3, GPIO_OUT);
+  gpio_set_dir(LED_PIN_4, GPIO_OUT);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+
+  gpio_pull_up(BTN_PIN_1);
 
   while (true) {
-    if (!gpio_get(BTN_PIN1)) {
-      gpio_put(LED_PIN1, 1);
-      sleep_ms (300);
-      
-      gpio_put(LED_PIN1, 0);
-      gpio_put(LED_PIN2, 1);
-      sleep_ms (300);
-      
-      gpio_put(LED_PIN2, 0);
-      gpio_put(LED_PIN3, 1);
-      sleep_ms (300);
-      
-      gpio_put(LED_PIN3, 0);
-      gpio_put(LED_PIN4, 1);
-      sleep_ms (300);
-      
-      gpio_put(LED_PIN4, 0);
-    }
+    if (!gpio_get(BTN_PIN)) {
+      gpio_put(LED_PIN_1, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_1, 0);
+      gpio_put(LED_PIN_2, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_2, 0);
+      gpio_put(LED_PIN_3, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_3, 0);
+      gpio_put(LED_PIN_4, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_4, 0);
+
+      while (!gpio_get(BTN_PIN)) {
+      };
+    };
   }
 }
